f76359fa63be1824e40c603dff592316e6dc30dc
lighteagle <sofyan.agus18@gmail.com>
2019-11-07 22:56:51 +0700

    update

--- index.js
+++ index.js
@@ -1,47 +1,55 @@
-let todoInput = document.getElementById('newTodo')
-let incompleteTodo = document.getElementById('incomplete-todo')
+let todoInput = document.getElementById("newTodo");
+let incompleteTodo = document.getElementById("incomplete-todo");
 
-
-const createTodoElement = (newTodoText) => {
+const createTodoElement = newTodoText => {
   // Create newElement :  ListItem, Checkbox, text(editInput), button(edit), button(delete)
-  let listItem = document.createElement('li')
-  let checkbox = document.createElement('input')
-  let label = document.createElement('label')
-  let editButton = document.createElement('button')
-  let deleteButton = document.createElement('button')
+  let listItem = document.createElement("li");
+  // let checkbox = document.createElement("input");
+  let label = document.createElement("label");
+  let editButton = document.createElement("button");
+  let deleteButton = document.createElement("button");
 
   // Modifiy newElement
-  checkbox.type = 'checkbox'
+  // checkbox.type = "checkbox";
 
-  editButton.innerText = 'Edit'
-  editButton.class = 'edit'
-  label.className = 'label-todo'
+  editButton.innerText = "Edit";
+  editButton.class = "edit";
+  label.className = "label-todo";
 
-  deleteButton.innerText = 'Delete'
-  deleteButton.className = 'Delete'
-  deleteButton.setAttribute("onclick", 'this.parentNode.remove()')
+  deleteButton.innerText = "Delete";
+  deleteButton.className = "Delete";
+  deleteButton.setAttribute("onclick", "this.parentNode.remove()");
 
-  label.innerText = newTodoText
-  label.className = "strikethrough"
+  label.innerText = newTodoText;
+  label.className = "noStrikethrough";
+
+  label.setAttribute(
+    "onclick",
+    "this.className==='strikethrough'?this.className='noStrikethrough':this.className='strikethrough';localStorage['list'] = incompleteTodo.innerHTML"
+  );
   // Append newElement
-  listItem.appendChild(checkbox)
-  listItem.appendChild(label)
-  listItem.appendChild(deleteButton)
+  // listItem.appendChild(checkbox);
+  listItem.appendChild(label);
+  listItem.appendChild(deleteButton);
 
-  return listItem
-}
+  return listItem;
+};
 
 //Add a new todo
 const addTodoList = () => {
-  event.preventDefault()
+  event.preventDefault();
   // create new list item with the text from #newTodo
   if (todoInput.value) {
-    let listItem = createTodoElement(todoInput.value)
-    incompleteTodo.appendChild(listItem)
-    todoInput.value = ''
+    let listItem = createTodoElement(todoInput.value);
+    incompleteTodo.appendChild(listItem);
+    todoInput.value = "";
+    localStorage["list"] = incompleteTodo.innerHTML;
   } else {
-    alert('Please input todo!')
+    alert("Please input todo!");
   }
-}
+};
 
-addTodoBtn.onclick = addTodoList
+if (localStorage["list"]) {
+  incompleteTodo.innerHTML = localStorage["list"];
+}
+addTodoBtn.onclick = addTodoList;
